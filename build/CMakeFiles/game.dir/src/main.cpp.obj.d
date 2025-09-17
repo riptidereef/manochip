5ba72fbfb0fb7b4df05acd6d577731f9cdcaf203
RiptideReef <jditzel02@gmail.com>
2025-09-17 03:59:29 -0400

    Setting up project and linking Raylib.

---
+++ build/CMakeFiles/game.dir/src/main.cpp.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/game.dir/src/main.cpp.obj: \
+ C:\Users\jditz\Desktop\GameDev\Emulator\manochip\src\main.cpp \
+ C:/Users/jditz/Desktop/GameDev/Emulator/manochip/lib/raylib/src/raylib.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/stdarg.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/stdarg.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/_mingw_stdarg.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/vadefs.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/_mingw.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/_mingw_mac.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/_mingw_secapi.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/PROGRA~2/I686-8~1.0-R/mingw32/i686-w64-mingw32/include/sdks/_mingw_ddk.h
